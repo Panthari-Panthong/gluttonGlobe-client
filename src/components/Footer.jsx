19a0f8920914a39b66cbccbe28e53d45219730a0
solenwa <solen.wanono@gmail.com>
2023-05-25 13:13:58 +0200

    Homepage styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,32 +5,30 @@ import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <>
-      <footer>
-        <div className="footer">
-          <div className="row">
-            <div className="Link">
-              <Link to="#">
-                <FontAwesomeIcon icon={faFacebook} />
-              </Link>
-            </div>
-            <div className="Link">
-              <Link to="#">
-                <FontAwesomeIcon icon={faInstagram} />
-              </Link>
-            </div>
+      <div className="footer">
+        <div className="row">
+          <div className="Link">
+            <Link to="#">
+              <FontAwesomeIcon icon={faFacebook} />
+            </Link>
           </div>
-          {/* <div className="row">
+          <div className="Link">
+            <Link to="#">
+              <FontAwesomeIcon icon={faInstagram} />
+            </Link>
+          </div>
+        </div>
+        {/* <div className="row">
             <ul>
               <li>
                 <Link to="#">Contact us</Link>
               </li>
             </ul>
           </div> */}
-          <div className="row">
-            <p>© 2023 Gluttonglobe - All rights reserved</p>
-          </div>
+        <div className="row">
+          <p>© 2023 Gluttonglobe - All rights reserved</p>
         </div>
-      </footer>
+      </div>
     </>
   );
 }
